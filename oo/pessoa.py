56010a52d9d19c43a7b6dd0844f12c1dba9eece0
Jean Pierre <75619077+jeanpva@users.noreply.github.com>
2020-12-07 19:38:22 -0300

    criados atributos de instãncia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 57):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa
+    p = Pessoa ('Luciana')
     print(Pessoa.cumprimentar(p))
     print (id(p))
-
+    p.nome ='Jean'
+    print (p.nome)
+    print (p.idade)
